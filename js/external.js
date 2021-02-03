bb2b2c3f2ad376310994cc2a8d2bd1e560958db8
Kenyon Luce <kenyon.luce02@gmail.com>
2021-02-03 10:01:35 -0600

    feat: defined moreThan2 and classFull variables

--- js/external.js
+++ js/external.js
@@ -24,23 +24,30 @@ alert("Total cost is $" + totalRent);
 var google = parseInt(prompt("How many hours did you work at Google this week?"));
 var facebook = parseInt(prompt("How many hours did you work at Facebook this week?"));
 var amazon = parseInt(prompt("How many hours did you work at Amazon this week?"));
-var totalPay = (400 * google) + (350 * facebook) + (380 * amazon);
-alert("You have earned $" + totalPay + " this week!");
+var totalPay = "$" + (400 * google) + (350 * facebook) + (380 * amazon);
+alert("You have earned " + totalPay + " this week!");
 
 //A student can be enrolled in a class only if the class is not full
-//and the class schedule does not conflict with her current schedule.
+//and the class schedule does NOT conflict with her current schedule.
 
-var classFull = confirm("Please click ok if your class is not full");
-var classSchedule = confirm("Please click ok if the class schedule does not conflict with your current schedule");
-alert("Can be enrolled? " + (classFull && classSchedule));
+var classSize = prompt("How many students are in the class you are enrolling for?");
+var classNotFull = classSize < 25;
+var classSchedule = confirm("Please click ok if you already have a class at 1:30pm");
+//we DO NOT want the student to have a class at 1:30 already, because it would conflict
+//with the class he/she is enrolling for, so if they do currently have a class at 1:30pm,
+//we want the boolean value of false instead of true, hence the exclamation mark
+alert("Can be enrolled? " + (classNotFull && !classSchedule));
 
 //A product offer can be applied only if
 //a person buys more than 2 items, and the offer has not expired.
 //Premium members do not need to buy a specific amount of products.
 
-var notExpired = confirm("Please click ok if the offer has not expired");
+var notExpired = confirm("Please click ok if the offer is still valid");
 var premiumMember = confirm("Please click ok if you are a Premium member");
-var moreThan2 = confirm("Please click ok if you have bought more than 2 items");
+var items = prompt("How many items have you bought?");
+var moreThan2 = parseInt(items) >= 2;
+//as long as the offer is not expired (still valid), the offer can be applied for
+//Premium members OR people who bought more than 2 items
 alert("Can apply offer? " + (notExpired && (premiumMember || moreThan2)));
 
 
--- js/js-in-html-external.js
+++ js/js-in-html-external.js
@@ -1,1 +1,16 @@
-console.log("Hello from external JS file")
+"use strict";
+console.log("Hello from external JS file");
+
+name = "Kenyon";
+console.log("Hello " + name);
+
+//Alert
+alert("Welcome to my site!");
+
+//Confirm
+var confirmLeave = confirm("Are you sure you want to leave?");
+console.log("confirmLeave " + confirmLeave);
+
+//Prompt
+var favColor = prompt("What is your favorite color?");
+console.log("Your favorite color is " + favColor);
