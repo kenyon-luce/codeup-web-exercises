7724478310e51f0c606dff3271d284b3d7877279
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-01 14:58:37 -0500

    updated exercise

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -2,8 +2,9 @@ console.log("js connected");
 
 $(document).ready(function(){
     console.log("Fully loaded and ready to go")
-    console.log("this code is still in ready function, goes last")
+    console.log("this code is still in ready function, goes last");
+    alert("DOM has finished loading");
 });
-//
+//^runs this code last after everything else
 
-console.log("Hello, jQuery!")
+console.log("Hello, jQuery!");
