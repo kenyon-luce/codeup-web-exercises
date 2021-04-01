2bae1d949e26bf15a33c0736bca97b95ed5e06c2
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-01 14:51:11 -0500

    feat: added jquery-exercises

---
+++ js/jquery-exercises.js
@@ -0,0 +1,9 @@
+console.log("js connected");
+
+$(document).ready(function(){
+    console.log("Fully loaded and ready to go")
+    console.log("this code is still in ready function, goes last")
+});
+//
+
+console.log("Hello, jQuery!")
