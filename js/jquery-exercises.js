f7ebad2f2ce2befab7363d61bb516404001af340
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-05 16:37:29 -0500

    uploaded team code and updated exercise

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,10 +1,29 @@
-console.log("js connected");
+// console.log("js connected");
+//
+// $(document).ready(function(){
+//     console.log("Fully loaded and ready to go")
+//     console.log("this code is still in ready function, goes last");
+//     alert("DOM has finished loading");
+// });
+// //^runs this code last after everything else
+//
+// console.log("Hello, jQuery!");
 
-$(document).ready(function(){
-    console.log("Fully loaded and ready to go")
-    console.log("this code is still in ready function, goes last");
-    alert("DOM has finished loading");
-});
-//^runs this code last after everything else
-
-console.log("Hello, jQuery!");
+//below is what we did as a team
+(function() {
+    $(document).ready(function() {
+        alert($('#heading-1').html());
+        var bestCharacter = $('#baller').html();
+        alert(`this is by far the best character: ${bestCharacter}!`)
+        $('#heading-1').css('background-color', 'yellow');
+        $('#heading-1').css('color', 'rebeccapurple');
+        $('#heading-1').css('text-align', 'center');
+        $('#character-list').css('font-family', 'Orbitron');
+        $('#loser').css('text-decoration', 'underline');
+        $('#body').css('background-image', 'url(https://gray-wdbj-prod.cdn.arcpublishing.com/resizer/zHWV1QvKgK2FezH0pR5_zlwIhYM=/1200x675/smart/cloudfront-us-east-1.images.arcpublishing.com/gray/JCANUUGIONKE5KT7C6WG7WSCZM.jpg)');
+        $('#body').css('color', 'yellow');
+        $('#div').css('display', 'flex');
+        $('#div').css('flex-flow', 'row wrap');
+        $('#para-3, #para-2').css('margin', '5em');
+    });
+})()
