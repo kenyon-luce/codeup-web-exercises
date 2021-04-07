89483103c1723f9a5e8c141b4858ac30f93e10b7
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-07 11:28:05 -0500

    updated konami code, added lecture notes

--- js/essentialMethods-live.js
+++ js/essentialMethods-live.js
@@ -4,31 +4,51 @@
 //.html() - GET the HTML contents or SET the HTML contents
 
 //TODO: Let's get the HTML from the element with the ID of "best-seller-cont"
+console.log($("#best-seller-cont").html()); //get the html of this id
 
 //TODO together: Let's set up a string with HTML to replace this old list with our updated best sellers - FEDORAS have moved into the number one spot!
+var html = '';
+html = "<h4 class=\"best-seller-header\">Our Top Sellers</h4>" +
+    "<ol><li>Fedora Hats</li>" +
+    "<li>Baseball Hats</li>" +
+    "<li>Cowboy Hats</li></ol>"
+//^tag inside string, so the attribute property has to have backslash so that it is recognized within the string
 
 //TODO: Now, let's get back into that "best-seller-cont" and replace that HTML!
+$('#best-seller-cont').html(html); //SETTING the new HTML
 
 //TODO Together: Can we make a click event that changes "Howell's" to "Marco's" when we click on it? Help me out!
+
 
 //.css() - get info about a property or change a property
 
 //TODO: Let's see what the width of our "main-content" class is with .css()
+console.log($(".main-content").css("width")); //getting some information (one argument)
 
 //TODO Together: Can you help me? I'd like to set my modal's text to have a cool color :)
+$(".model-body").css("color", "red") //setting CSS (two arguments)
 
 //TODO: Let's make an object for us to use with .css(). .
+var warningRed = {"background-color":"red", "font-size":"72px"}
 
 //TODO Together: Nice - now, let's test this out by targeting our h1 with this warning background! Next, let's chain a font color after it
+$("h1").css(warningRed).css("color", "cornflowerblue");
 
 //.addClass() ~ .removeClass() ~ .toggleClass()
 
 //TODO: We've used a lot of bootstrap on the page - let's see what we can use .addClass to do in combination with bootstrap. Let's UPPERCASE that h1 with the shiny red background!
+$('h1').addClass("text-uppercase");
 
 //TODO Together: Can you assist me in removing the "bg-dark" background from our FOOTER?
+$("footer").removeClass("bg-dark")
 
 //TODO: I have a custom css class known as hi-lite - .toggleClass() with a .click() would be a neat way to use it
-
+$('*').click(function(e){
+    e.preventDefault();
+    // alert("Clicked?");
+    var ele = $(e.target);
+    ele.toggleClass("hi-lite");
+})
 
 //Bonus activity:
 
