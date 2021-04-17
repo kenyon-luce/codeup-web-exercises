9eda633777d4c0dba1e08d176a56bdaf1359b31e
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-17 12:13:38 -0500

    updated exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -36,4 +36,57 @@ const users = [
         yearsOfExperience: 9
     }
 ];
-console.log(users)
+console.log(users);
+
+//TODO: Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+var threeLang = users.filter(function (user){
+  if(user.languages.length > 2){
+      return user.languages;
+  }
+})
+console.log(threeLang);
+
+//TODO: Use .map to create an array of strings where each element is a user's email address
+var userEmails = users.map(function(user){
+    return user.email;
+})
+console.log(userEmails);
+
+//TODO: Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+var totalExp = users.reduce(function(total, user){
+    return total + user.yearsOfExperience
+    //made the mistake of returning the average within this function, so it got the average each time it iterated through the array
+    //(0+5)/5 = 1 --> (1+6)/5 = 1.4 --> (1.4+7)/5 = 1.68 --> (1.68+8)/5 = 1.936 --> (1.936+9)/5 = 2.1872
+}, 0);
+var averageExp = totalExp/users.length
+console.log(averageExp);
+
+//TODO: Use .reduce to get the longest email from the list of users.
+
+var emails = []
+users.forEach(function(user){
+    emails.push(user.email)
+})
+console.log(emails)
+
+var longestEmail = emails.reduce(function(a,b){
+    if(a.length > b.length) {
+        return a
+    } else {
+        return b
+    }
+}, '')
+console.log(longestEmail)
+
+//TODO: Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+var userNames = users.reduce(function(total, user){
+    return total + user.name + ", "
+}, '')
+console.log(userNames)
+
+//TODO BONUS: Use .reduce to get the unique list of languages from the list of users.
+var languages = users.reduce(function(total, user){
+    return total + user.languages
+}, '')
+//use the "count words" function to grab and display the appearances of all the languages mentioned per user
+console.log(languages)
