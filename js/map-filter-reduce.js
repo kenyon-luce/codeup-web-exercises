9b79c253786ae5f2580b95233f3b80c963dd8136
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-18 14:24:15 -0500

    finished bonus

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -94,23 +94,25 @@ console.log(listUserNames + '.')
 
 var languages = []
 users.forEach(function(user){
-    languages.push(user.languages)
+    languages.push(user.languages.join(' '))
 })
-languages.join('')
+var languages = languages.join(' ')
+
 console.log(languages)
-// function countWords(sentence) {
-//     const words = sentence.split(' '); // transform a sentence into an array of words
-//     const wordCountObject = words.reduce((wordCounts, word) => {
-//         if (typeof wordCounts[word] === 'undefined') {
-//             // if the word is not yet present in our object, set it's value to 1
-//             wordCounts[word] = 1;
-//         } else {
-//             // otherwise increment the existing count
-//             wordCounts[word] += 1;
-//         }
-//         return wordCounts;
-//     }, {}); // start with an empty object
-//     return wordCountObject;
-// }
+
+function countWords(sentence) {
+    const words = sentence.split(' '); // transform a sentence into an array of words
+    const wordCountObject = words.reduce((wordCounts, word) => {
+        if (typeof wordCounts[word] === 'undefined') {
+            // if the word is not yet present in our object, set it's value to 1
+            wordCounts[word] = 1;
+        } else {
+            // otherwise increment the existing count
+            wordCounts[word] += 1;
+        }
+        return wordCounts;
+    }, {}); // start with an empty object
+    return wordCountObject;
+}
 //use the "count words" function to grab and display the appearances of all the languages mentioned per user
-// console.log(countWords(languages))
+console.log(countWords(languages))
